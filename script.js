bc0d39c8326246167eef781961ba76c82bd1778c
JoaoGero <joao.marques@gerodigital.pt>
2024-12-10 12:16:33 +0000

    Finish game logic
    Set the game to be fully functional and work as a best of 5.

--- script.js
+++ script.js
@@ -19,34 +19,62 @@ function getHumanChoice() {
 let humanScore = 0
 let computerScore = 0
 
-function playRound() {
+function incrementHuman() {
+    humanScore++;
+    return (humanScore);
+}
+
+function incrementComputer() {
+    computerScore++;
+    return (computerScore);
+}
+
+function score() {
+    if (humanScore < 3 && computerScore < 3) {
+        return ("Player " +humanScore+ "-" +computerScore+ " Computer")
+    }
+    else {
+        if (humanScore == 3) {
+            humanScore = 0;
+            computerScore = 0;
+            return ("Player Wins!")
+        }
+        else if (computerScore == 3) {
+            humanScore = 0;
+            computerScore = 0;
+            return ("Computer Wins!")
+        }
+    }
+}
+
+function playGame() {
     let humanChoice = getHumanChoice().toLowerCase();
     let computerChoice = getComputerChoice().toLowerCase();
     if (humanChoice == "rock" && computerChoice == "rock") {
-        return console.log("Draw! Rock ties Rock!");
+        return console.log("Draw! Rock ties Rock!"), score();
     }
     else if (humanChoice == "rock" && computerChoice == "paper") {
-        return console.log("Lose! Rock loses to Paper!");
+        return console.log("Lose! Rock loses to Paper!"), incrementComputer(), score();
     }
     else if (humanChoice == "rock" && computerChoice == "scissors") {
-        return console.log("Win! Rocks wins to Scissors");
+        return console.log("Win! Rocks wins to Scissors"), incrementHuman(), score();
     }
     else if (humanChoice == "paper" && computerChoice == "paper") {
-        return console.log("Draw! Paper ties Paper!");
+        return console.log("Draw! Paper ties Paper!"), score();
     }
     else if (humanChoice == "paper" && computerChoice == "scissors") {
-        return console.log("Lose! Paper loses to Scissors!");
+        return console.log("Lose! Paper loses to Scissors!"), incrementComputer(), score();
     }
     else if (humanChoice == "paper" && computerChoice == "rock") {
-        return console.log("Win! Paper wins to Rock!");
+        return console.log("Win! Paper wins to Rock!"), incrementHuman(), score();
     }
     else if (humanChoice == "scissors" && computerChoice == "scissors") {
-        return console.log("Tie! Scissors ties Scissors!");
+        return console.log("Tie! Scissors ties Scissors!"), score();
     }
     else if (humanChoice == "scissors" && computerChoice == "rock") {
-        return console.log("Lose! Scissors loses to Rock!");
+        return console.log("Lose! Scissors loses to Rock!"), incrementComputer(), score();
     }
     else {
-        return console.log("Win! Scissors wins to Paper!");
+        return console.log("Win! Scissors wins to Paper!"), incrementHuman(), score();
     }
 }
